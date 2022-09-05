68ac66a8e313d19341e5a062cbcfc47c77037fa1
Nimród Papp <nimrod.papp@liferay.com>
2022-09-05 17:34:12 +0200

    JS WS Beginner Homework Nimi

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 // DOM Manipulation
 
-testing
+// Variables
+const openBtn = document.getElementById('modal-btn');
+const modalContainer = document.getElementById('my-modal');
+const closeBtn = document.getElementsByClassName('close');
+
+// Event Listeners
+openBtn.addEventListener("click", function() {
+    modalContainer.style.display = 'block';
+});
+
+closeBtn[0].addEventListener("click", function() {
+    modalContainer.style.display = 'none';
+});
+
+window.addEventListener("click", function(e) {
+    if (e.target === modalContainer) {
+        modalContainer.style.display = 'none';
+    }
+});
