ee68711adc3e26321429e943fd9da63803109751
solyom-nagy <solyom.nagy@liferay.com>
2022-09-06 22:14:57 +0200

    I am finished with the exercise

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-asdasdasdasdasd;
-and again;
+
+const modalBtn = document.getElementById("modal-btn");
+const modalBox = document.getElementsByClassName("modal");
+const closeBtn = document.getElementsByClassName("close");
+
+function openModal (){
+    modalBox[0].style.display = 'block';
+}
+
+function closeModal (){
+    modalBox[0].style.display = 'none';
+}
+
+modalBtn.addEventListener("click", openModal);
+closeBtn[0].addEventListener("click", closeModal);
+
+function outsideClick(e) {
+    if (e.target == modalBox[0]) {
+      closeModal();
+    }
+}
+
+document.addEventListener("click", outsideClick);
