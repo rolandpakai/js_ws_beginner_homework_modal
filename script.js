6e9d892f81b8203050e87f10b68e04959c5ad667
unknown <gergely.szalay@liferay.com>
2022-09-07 18:46:38 +0200

    Homework done

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Hello World");
+const button2 = document.querySelector('.button');
+const xbutton = document.querySelector('.close');
+const module = document.querySelector('.modal');
+
+function revealContent() {
+    module.style.display = 'inline';
+}
+function hideContent() {
+    module.style.display = 'none';
+}
+
+document.addEventListener("click", function (e) {
+    const target = e.target;
+    if (target.matches('.button')) {
+        revealContent();
+    } else {
+        if (target.matches('.close')) {
+            hideContent();
+        } else {
+            if (target.matches('.modal')) {
+                hideContent();
+            }
+        }
+    }
+    console.log(target);
+}, true);
