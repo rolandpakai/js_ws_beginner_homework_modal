819159404baa6e70a768bf4d2cbb161b1de4a5a0
unknown <gergely.szalay@liferay.com>
2022-09-08 12:28:26 +0200

    Corrected HW

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
 const button2 = document.querySelector('.button');
 const xbutton = document.querySelector('.close');
 const module = document.querySelector('.modal');
-
 function revealContent() {
-    module.style.display = 'inline';
+    module.style.display = 'block';
 }
 function hideContent() {
     module.style.display = 'none';
 }
+module.addEventListener("click", hideContent);
+xbutton.addEventListener("click", hideContent);
+button2.addEventListener("click", revealContent);
 
-document.addEventListener("click", function (e) {
-    const target = e.target;
-    if (target.matches('.button')) {
-        revealContent();
-    } else {
-        if (target.matches('.close')) {
-            hideContent();
-        } else {
-            if (target.matches('.modal')) {
-                hideContent();
-            }
-        }
-    }
-    console.log(target);
-}, true);
+
+
