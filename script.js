2c0d8baf47ddeafc7685c5b52b4a78bea72e3845
unknown <odon.barta@liferay.com>
2022-09-22 18:39:39 +0200

    modal homework v.01

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-test
+// Get modal element
+var myModal = document.getElementById('my-modal')
+//Get open modal button
+var modalBtn = document.getElementById('modal-btn');
+// Get close button
+var closeBtn = document.getElementsByClassName('close')[0];
+
+// Listen for open click
+modalBtn.addEventListener('click', openModal);
+//Listen for close click
+closeBtn.addEventListener('click', closeModal);
+// Listen for outside click
+window.addEventListener('click', outsideClick);
+
+//Function to open modal
+function openModal(){
+    myModal.style.display = 'block';
+}
+
+//Function to close modal
+function closeModal() {
+    myModal.style.display = 'none';
+}
+
+//Function to close modal if outside click
+function outsideClick(e){
+    if(e.target == myModal){
+        myModal.style.display = 'none';
+    }
+}
+
